753bcf334f0d2a46709da889a3f52785990d3b78
7 <hackinitosh@qq.com>
2023-05-15 13:33:15 +0000

    ok

--- repo/update.sh
+++ repo/update.sh
@@ -7,12 +7,12 @@ set -e
 rm -f Packages*
 
 # 生成新 Packages 文件
-dpkg-scanpackages -m ./debs > Packages
+sudo dpkg-scanpackages -m ./debs > Packages
 
 # 压缩 Packages 文件
-gzip -c Packages > Packages.gz
+sudo gzip -c Packages > Packages.gz
 
 # 生成 Packages.bz2 文件
-bzip2 -k Packages > Packages.bz2
+sudo bzip2 -k Packages > Packages.bz2
 
 echo "Packages 命令执行成功！"
