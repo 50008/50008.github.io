1cbb0fa8486929b959c043d97c2a5dbe1b98a2e3
7 <hackinitosh@qq.com>
2023-05-15 12:16:41 +0000

    ok

--- repo/dpkg_packages.sh
+++ repo/dpkg_packages.sh
@@ -1,5 +1,7 @@
 rm -f Packages
 rm -f Packages.bz2
 dpkg-scanpackages -m ./debs > Packages
+gzip -c Packages > Packages.gz
 bzip2 -k Packages
-echo "!success!"
+echo "命令执行成功"
+# sudo sh ./dpkg_packages.sh
