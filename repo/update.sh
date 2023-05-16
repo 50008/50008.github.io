4992889e96d988c786902a050cb18ec506daa268
7 <hackinitosh@qq.com>
2023-05-16 04:10:22 +0000

    OK

--- repo/update.sh
+++ repo/update.sh
@@ -1,6 +1,8 @@
-# sudo sh ./dpkg_packages.sh
+# sudo sh ./dpkg_packages.sh   
+# sudo sh update.sh
 set -e
 rm -f Packages*
+
 sudo dpkg-scanpackages -m debs/ > Packages
 bzip2 -c Packages > Packages.bz2
 gzip -c Packages > Packages.gz
