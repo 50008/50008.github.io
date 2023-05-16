c75cbb68ac941910803a9ffb3beb8ce62fce5f98
7 <hackinitosh@qq.com>
2023-05-16 03:49:07 +0000

    ok

--- repo/update.sh
+++ repo/update.sh
@@ -1,18 +1,6 @@
 # sudo sh ./dpkg_packages.sh
-# 
-#!/bin/bash
 set -e
-
-# 清除旧文件
 rm -f Packages*
-
-# 生成新 Packages 文件
 sudo dpkg-scanpackages -m debs/ > Packages
-
-# 压缩 Packages 文件
-sudo gzip -c Packages > Packages.gz
-
-# 生成 Packages.bz2 文件
-sudo bzip2 -k Packages
-
-echo "Packages 命令执行成功！"
+bzip2 -c Packages > Packages.bz2
+gzip -c Packages > Packages.gz
